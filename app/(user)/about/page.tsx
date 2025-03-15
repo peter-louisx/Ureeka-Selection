3337cc7dafc7e63ff00db201685b0acc9317ebdc
AndrewLorenzo <vincentius.lorenzo@binus.ac.id>
2025-03-15 23:49:30 +0700

    about page
    nambahin gambar ama tengahin tulisan

--- app/(user)/about/page.tsx
+++ app/(user)/about/page.tsx
@@ -29,19 +29,18 @@ export default function About() {
   return (
     <>
      <div className="relative h-[300px] md:h-[400px] overflow-hidden rounded-lg mx-4 mt-4">
-  {/* <img
-    src="/about.png"
-    alt="Farmers working in field"
-    className="w-full h-full object-cover"
-  /> */}
-  <div className="absolute inset-0 bg-green-50 p-6 flex flex-col justify-end">
-    <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-2">We are CropFresh</h1>
-    <span className="text-xl md:text-2xl font-bold text-gray-800 mb-2">
-      CropFresh connects hardworking producers with honest clients, anywhere, anytime. We ensure high-quality
-      produce reaches consumers in a fair and transparent way.
-    </span>
-  </div>
-</div>
+        <img
+          src="./about.png"
+          alt="Farmers working in field"
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute inset-0 bg-opacity-30 p-6 flex flex-col justify-center items-center">
+          <h1 className="text-4xl md:text-5xl font-bold text-white mb-2">We are CropFresh</h1>
+          <span className="text-xl md:text-3xl font-bold text-white mb-2">
+            Our job is connecting hard working producers with good, honest clients anywhere, everywhere.
+          </span>
+        </div>
+    </div>
 
 
       {/* Mission Section */}
